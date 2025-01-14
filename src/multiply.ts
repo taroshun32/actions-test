867322c95e56147d5012d1e1288e88166ce97b35
taroshun32 <shunta.nakamura@nextbeat.net>
2025-01-14 19:28:37 +0900

    update code

--- src/multiply.ts
+++ src/multiply.ts
@@ -1,4 +1,3 @@
 export function multiply(a: number, b: number): number {
-  console.log('multiply');
   return a * b;
 }
--- src/subtract.ts
+++ src/subtract.ts
@@ -1,4 +1,3 @@
 export function subtract(a: number, b: number): number {
-  console.log('subtract');
   return a - b;
 }
